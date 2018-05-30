9de90613bc622b49b8286457cd070fb759ba9d4c
Krishna305_ <arjunlama798@gmail.com>
2018-05-30 11:33:21 +0545

    first commit

---
+++ JWT1/JWT1/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWT1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+      
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+         
+        }
+    }
+}
---
+++ JWT1/JWT1/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using JWT1.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.KeyVault.Models;
+
+namespace JWT1.Controllers
+{
+     [Route("api/[controller]")] 
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _appDbContext;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+        public AccountController(UserManager<AppUser> userManager, IMapper mapper, ApplicationDbContext appDbContext)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+            _appDbContext = appDbContext;
+        }
+
+     
+
+        // POST api/account
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]RegistrationViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userIdentity = _mapper.Map<AppUser>(model);
+
+            var result = await _userManager.CreateAsync(userIdentity, model.Password);
+
+            if (!result.Succeeded) return Content("not succeed");
+
+            await _appDbContext.Customers.AddAsync(new Customer { IdentityId = userIdentity.Id, Location = model.Location });
+            await _appDbContext.SaveChangesAsync();
+
+            return new OkObjectResult("Account created");
+        }
+    }
+}
